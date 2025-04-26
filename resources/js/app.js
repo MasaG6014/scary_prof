45ff5b0dd56b1c3f3905e062030e0219acf7db0e
MasaG6014 <g2002masa@gmail.com>
2025-04-26 14:39:08 +0900

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import HelloMessage from './components/HelloMessage.vue'
+
+createApp(HelloMessage).mount('#app')
---
+++ resources/js/components/HelloMessage.vue
@@ -0,0 +1,8 @@
+<script setup>
+import axios from 'axios'
+
+const fetchHello = async () => {
+  const res = await axios.get('/api/hello')
+  console.log(res.data.message)
+}
+</script>
