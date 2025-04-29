b123aa0ff27415b073510c75aba965872d591bfb
MasaG6014 <g2002masa@gmail.com>
2025-04-29 23:07:31 +0900

    overall api done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Cache;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        
+        // ローカル環境でのみ、かつ初回リクエスト時に Redis 初期化処理を行う例
+        if ($this->app->environment('local')) {
+            $flagPath = storage_path('framework/redis_initialized');
+            if (!file_exists($flagPath)) { // 初回実行の場合のみ
+                Redis::set('vote:access', 0);
+                Redis::set('vote:start', 0);
+                Cache::put('pkList',[], 3600);
+                // 必要に応じて他のキーの初期化も行う
+                file_put_contents($flagPath, '1'); // 初期化済みフラグを作成
+            }
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,44 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Http\Request;
 
 Route::get('/hello', function () {
     return response()->json(['message' => 'hello world']);
 });
 
-Route::post('/vote/access', function () {
+Route::post('/vote/access', function (Request $request) {
     Redis::incr('vote:access');
+    $data = $request->all();
+    $pk = $data['pk'];
+    $pkList = Cache::get('pkList');
+    $pkList = $pk;
+    Cache::put($pk, [], 3600);
+    Cache::put('pkList', $pkList, 3600);
     return response()->json(['status' => 'ok']);
+});
+
+Route::get('/vote/getPkList', function (Request $request) {
+    $pkList = Cache::get('pkList');
+    return response()->json(['pkList' => $pkList]);
+});
+
+Route::post('/vote/postShares', function (Request $request) {
+    $data = $request->all();
+    $shares = $data['shares'];
+    foreach ($shares as $pk => $share) {
+        $shareList = Cache::get($pk);
+        $shareList = $share;
+        Cache::put($pk, $shareList, 3600);
+    }
+    Redis::incr('vote:tallyReady');
+});
+
+Route::get('vote/tally', function(Request $request) {
+    $data = $request->all();
+    $pk = $data['pk'];
+    $shares = Cache::get($pk);
+    return response() -> json(['shares' => $shares]);
 });
 
 Route ::get('/vote/count', function () {
@@ -22,7 +52,17 @@ Route::post('/vote/leave', function () {
     return response()->json(['status' => 'ok']);
 });
 
-Route::post('manage/reset', function () {
+Route::post('/manage/reset', function () {
     Redis::set('vote:access', 0);
+    return response()->json(['status' => 'ok']);
+});
+
+Route::post('/manage/start', function () {
+    Redis::set('vote:start', 1);
+    return response()->json(['status' => 'ok']);
+});
+
+Route::post('/manage/tally', function () {
+    Redis::set('vote:start',0);
     return response()->json(['status' => 'ok']);
 });
